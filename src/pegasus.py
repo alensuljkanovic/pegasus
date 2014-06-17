1f928044e6d402b12edadd7c317f7365b5229966
Alen Suljkanovic <alienized91 AT gmail DOT com>
2014-06-17 21:00:27 +0200

    Implemented solving of left recursion.

--- src/pegasus.py
+++ src/pegasus.py
@@ -33,7 +33,8 @@ class Pegasus(object):
                 else:
                     if option.find('|') != -1:
                         alternatives = option.split('|')
-                        seq = self.create_peg_sequence(alternatives)
+                        recursive = self.avoid_left_recursion(rule_name, alternatives)
+                        seq = self.create_peg_sequence(rule_name, alternatives, recursive)
                         #print(seq)
                     elif option.find(' ') != -1:
                         alternatives = option.split(' ')
@@ -42,12 +43,17 @@ class Pegasus(object):
             rule = Rule(rule_name, seq)
             print(rule.name + ' : ' + rule.alternatives)
 
-    def create_peg_sequence(self, alternatives):
+    def create_peg_sequence(self, rule_name, alternatives, recursive):
         sequence = '['
         count = len(alternatives)
+        if recursive is not None:
+            sequence = alternatives[0].strip() + ', ' + recursive
+            return sequence
+
         for i, alt in enumerate(alternatives):
             #Trimming whitespaces
             alt = alt.strip()
+
             comment_index = alt.find('//')
             if comment_index != -1:
                 alt = alt[0:comment_index]
@@ -94,24 +100,199 @@ class Pegasus(object):
         ordered += ')'
         return ordered
 
+    def avoid_left_recursion(self, rule_name, alternatives):
+        operators = []
+        for i, alt in enumerate(alternatives):
+            alt = alt.strip()
+            index = alt.find(rule_name)
+            if index == 0:
+                alt = alt[len(rule_name): len(alt)].strip()
+                index2 = alt.find(' ')
+                operator = alt[0: index2]
+                operators.append(operator)
+
+        ret_val = 'ZeroOrMore(['
+
+        length = len(operators)
+        if length == 0:
+            return None
+
+        for i, op in enumerate(operators):
+            ret_val += op
+            if i != length - 1:
+                ret_val += ','
+
+        ret_val += '],  ' + alternatives[0].strip() + ')'
+        return ret_val
+
+
 if __name__ == "__main__":
     test_data = """
 
-            enumSpecifier
-            : 'enum' Identifier? '{' enumeratorList '}'
-            | 'enum' Identifier? '{' enumeratorList ',' '}'
-            | 'enum' Identifier
-            ;
+          unaryOperator
+: '&' | '*' | '+' | '-' | '~' | '!'
+;
 
-            enumeratorList
-            : enumerator
-            | enumeratorList ',' enumerator
-            ;
+castExpression
+: unaryExpression
+| '(' typeName ')' castExpression
+| '__extension__' '(' typeName ')' castExpression
+;
 
-            enumerator
-            : enumerationConstant
-            | enumerationConstant '=' constantExpression
-            ;
+multiplicativeExpression
+: castExpression
+| multiplicativeExpression '*' castExpression
+| multiplicativeExpression '/' castExpression
+| multiplicativeExpression '%' castExpression
+;
+
+additiveExpression
+: multiplicativeExpression
+| additiveExpression '+' multiplicativeExpression
+| additiveExpression '-' multiplicativeExpression
+;
+
+shiftExpression
+: additiveExpression
+| shiftExpression '<<' additiveExpression
+| shiftExpression '>>' additiveExpression
+;
+
+relationalExpression
+: shiftExpression
+| relationalExpression '<' shiftExpression
+| relationalExpression '>' shiftExpression
+| relationalExpression '<=' shiftExpression
+| relationalExpression '>=' shiftExpression
+;
+
+equalityExpression
+: relationalExpression
+| equalityExpression '==' relationalExpression
+| equalityExpression '!=' relationalExpression
+;
+
+andExpression
+: equalityExpression
+| andExpression '&' equalityExpression
+;
+
+exclusiveOrExpression
+: andExpression
+| exclusiveOrExpression '^' andExpression
+;
+
+inclusiveOrExpression
+: exclusiveOrExpression
+| inclusiveOrExpression '|' exclusiveOrExpression
+;
+
+logicalAndExpression
+: inclusiveOrExpression
+| logicalAndExpression '&&' inclusiveOrExpression
+;
+
+logicalOrExpression
+: logicalAndExpression
+| logicalOrExpression '||' logicalAndExpression
+;
+
+conditionalExpression
+: logicalOrExpression ('?' expression ':' conditionalExpression)?
+;
+
+assignmentExpression
+: conditionalExpression
+| unaryExpression assignmentOperator assignmentExpression
+;
+
+assignmentOperator
+: '=' | '*=' | '/=' | '%=' | '+=' | '-=' | '<<=' | '>>=' | '&=' | '^=' | '|='
+;
+
+expression
+: assignmentExpression
+| expression ',' assignmentExpression
+;
+
+constantExpression
+: conditionalExpression
+;
+
+declaration
+: declarationSpecifiers initDeclaratorList? ';'
+| staticAssertDeclaration
+;
+
+declarationSpecifiers
+: declarationSpecifier+
+;
+
+declarationSpecifiers2
+: declarationSpecifier+
+;
+
+declarationSpecifier
+: storageClassSpecifier
+| typeSpecifier
+| typeQualifier
+| functionSpecifier
+| alignmentSpecifier
+;
+
+initDeclaratorList
+: initDeclarator
+| initDeclaratorList ',' initDeclarator
+;
+
+initDeclarator
+: declarator
+| declarator '=' initializer
+;
+
+storageClassSpecifier
+: 'typedef'
+| 'extern'
+| 'static'
+| '_Thread_local'
+| 'auto'
+| 'register'
+;
+
+typeSpecifier
+: ('void'
+| 'char'
+| 'short'
+| 'int'
+| 'long'
+| 'float'
+| 'double'
+| 'signed'
+| 'unsigned'
+| '_Bool'
+| '_Complex'
+| '__m128'
+| '__m128d'
+| '__m128i')
+| '__extension__' '(' ('__m128' | '__m128d' | '__m128i') ')'
+| atomicTypeSpecifier
+| structOrUnionSpecifier
+| enumSpecifier
+| typedefName
+| '__typeof__' '(' constantExpression ')' // GCC extension
+;
+
+structOrUnionSpecifier
+: structOrUnion Identifier? '{' structDeclarationList '}'
+| structOrUnion Identifier
+;
+
+structOrUnion
+: 'struct'
+| 'union'
+;
+
+
         """
     pegasus = Pegasus(test_data, True)
     pegasus.parse_cfg()
